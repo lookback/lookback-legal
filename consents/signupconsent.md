080ea7a7593220b738ee8662038a5a40610617c9
Johan Brook <johan@lookback.io>
2018-04-30 14:07:10 +0200

    Initial commit

---
+++ consents/signupconsent.md
@@ -0,0 +1,3 @@
+Some consent.
+
+Has multiple **lines**.
---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+
+function findConsentsFromFile(pattern) {
+  return glob.sync(pattern)
+    .map(filename => ({
+      name: path.basename(filename).split('.')[0],
+      text: fs.readFileSync(filename, { encoding: 'utf8' })
+    }))
+    .reduce((acc, consent) => {
+      acc[consent.name] = consent.text;
+      return acc;
+    }, {});
+}
+
+module.exports = findConsentsFromFile('consents/*.md');
