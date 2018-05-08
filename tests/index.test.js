b03667beaa680c15a21009e104859c2088fe4ef0
Johan Brook <johan@lookback.io>
2018-05-08 16:49:59 +0200

    Be vain and style the test output a bit

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,98 +1,117 @@
-const { test } = require('./test-runner');
+const { test, suite } = require('./test-runner');
 
-require('./included-consents.test');
+suite('All consents', () => {
+  require('./included-consents.test');
+});
 
 // Under test
 const { userHasConsented, findConsent, Consents } = require('../')(`${__dirname}/fixtures`);
 
-// ------ Test the data structure
+suite('Index', () => {
 
-test('All consents are grouped on consent name and sorted in descending order by date', t => {
-  // Check that the two directories in fixtures exist as top level "groups":
-  t.ok(Consents.testconsent, 'testconsent exists');
-  t.ok(Consents.someotherconsent, 'someotherconsent exists');
+  // ------ Test the data structure
 
-  // Simple sort check to see that the first element's
-  // createdAt is before the next one
-  t.ok(Consents.testconsent[0].createdAt.getTime() > Consents.testconsent[1].createdAt.getTime()
-    , 'sorted latest to oldest');
-});
+  test('All consents are grouped on consent name and sorted in descending order by date', t => {
+    // Check that the two directories in fixtures exist as top level "groups":
+    t.ok(Consents.testconsent, 'testconsent exists');
+    t.ok(Consents.someotherconsent, 'someotherconsent exists');
 
-// ------ userHasConsented
+    // Simple sort check to see that the first element's
+    // createdAt is before the next one
+    t.ok(Consents.testconsent[0].createdAt.getTime() > Consents.testconsent[1].createdAt.getTime()
+      , 'sorted latest to oldest');
+  });
 
-test('userHasConsented returns false if user has no consents', t => {
-  const user = {};
+  // ------ userHasConsented
 
-  t.equal(userHasConsented(user, 'testconsent'), false);
-});
+  test('userHasConsented returns false if user has no consents', t => {
+    const user = {};
 
-test('userHasConsented returns true if user has consented to latest version', t => {
-  const user = {
-    consents: [
-      {
-        consentName: 'testconsent',
-        version:     '9bcbcc8ddd742d0386cda25501786914e5a57a82b7fbfa5e5ab1b488807ec3ca'
-      }
-    ]
-  };
+    t.equal(userHasConsented(user, 'testconsent'), false);
+  });
 
-  t.equal(userHasConsented(user, 'testconsent'), true);
-});
+  test('userHasConsented returns true if user has consented to latest version', t => {
+    const user = {
+      consents: [
+        {
+          consentName: 'testconsent',
+          version:     '9bcbcc8ddd742d0386cda25501786914e5a57a82b7fbfa5e5ab1b488807ec3ca'
+        }
+      ]
+    };
 
-// XXX Is this function supposed to work like this?
-test('userHasConsented returns false if user has not consented to latest version', t => {
-  const user = {
-    consents: [
-      {
-        consentName: 'testconsent',
-        version:     '008d3f71f1a0367205d95759d7ef4fbcf67420e7219026470d278dd73003a243'
-      }
-    ]
-  };
+    t.equal(userHasConsented(user, 'testconsent'), true);
+  });
 
-  t.equal(userHasConsented(user, 'testconsent'), false);
-});
+  // XXX Is this function supposed to work like this?
+  test('userHasConsented returns false if user has not consented to latest version', t => {
+    const user = {
+      consents: [
+        {
+          consentName: 'testconsent',
+          version:     '008d3f71f1a0367205d95759d7ef4fbcf67420e7219026470d278dd73003a243'
+        }
+      ]
+    };
 
-test('userHasConsented returns true if user has consented to specific version', t => {
-  const version = '008d3f71f1a0367205d95759d7ef4fbcf67420e7219026470d278dd73003a243';
+    t.equal(userHasConsented(user, 'testconsent'), false);
+  });
 
-  const user = {
-    consents: [
-      {
-        consentName: 'testconsent',
-        version
-      }
-    ]
-  };
+  test('userHasConsented returns false if user has not consented to the specified consent', t => {
+    const user = {
+      consents: [
+        {
+          consentName: 'randomconsent',
+          version:     'aadd3f71f1a0367205d95759d7ef4fbcf67420e7219026470d278dd73003a241'
+        }
+      ]
+    };
 
-  t.equal(userHasConsented(user, 'testconsent', version), true);
-});
+    t.equal(userHasConsented(user, 'testconsent'), false);
+  });
 
-// ------ findConsent
+  test('userHasConsented returns true if user has consented to specific version', t => {
+    const version = '008d3f71f1a0367205d95759d7ef4fbcf67420e7219026470d278dd73003a243';
 
-test('findConsent without specifying returns the latest consent', t => {
-  const latest = Consents.testconsent[0];
+    const user = {
+      consents: [
+        {
+          consentName: 'testconsent',
+          version
+        }
+      ]
+    };
 
-  t.ok(findConsent('testconsent'), 'returns latest');
-  t.equal(findConsent('testconsent').version, latest.version, 'the versions match');
-});
+    t.equal(userHasConsented(user, 'testconsent', version), true);
+  });
 
-test('findConsent can return a specific version', t => {
-  const version = '008d3f71f1a0367205d95759d7ef4fbcf67420e7219026470d278dd73003a243';
-  const consent = findConsent('testconsent', version);
+  // ------ findConsent
 
-  t.ok(consent, 'returns specific version');
-  t.equal(consent.version, version, 'the versions match');
-});
+  test('findConsent without specifying returns the latest consent', t => {
+    const latest = Consents.testconsent[0];
 
-test('findConsent returns null for non existing consent name', t => {
-  const consent = findConsent('herpderp');
+    t.ok(findConsent('testconsent'), 'returns latest');
+    t.equal(findConsent('testconsent').version, latest.version, 'the versions match');
+  });
 
-  t.equal(consent, null, 'did not find consent');
-});
+  test('findConsent can return a specific version', t => {
+    const version = '008d3f71f1a0367205d95759d7ef4fbcf67420e7219026470d278dd73003a243';
+    const consent = findConsent('testconsent', version);
 
-test('findConsent returns null for non existing version', t => {
-  const consent = findConsent('testconsent', 'foo');
+    t.ok(consent, 'returns specific version');
+    t.equal(consent.version, version, 'the versions match');
+  });
 
-  t.equal(consent, null, 'did not find consent version');
+  test('findConsent returns null for non existing consent name', t => {
+    const consent = findConsent('herpderp');
+
+    t.equal(consent, null, 'did not find consent');
+  });
+
+  test('findConsent returns null for non existing version', t => {
+    const consent = findConsent('testconsent', 'foo');
+
+    t.equal(consent, null, 'did not find consent version');
+  });
+
 });
--- tests/test-runner.js
+++ tests/test-runner.js
@@ -1,11 +1,19 @@
 const assert = require('assert');
+const colors = require('colors/safe');
+
+exports.suite = function testSuite(name, cb) {
+  console.log(`\n\n${colors.gray('-'.repeat(50))}\n`);
+  console.log(`   ${colors.gray('Testing')} ${name}\n`);
+  cb();
+};
 
 exports.test = function test(title, cb) {
   try {
     cb(assert);
-    console.log(`\t✔︎ ${title}`);
+    console.log(`   ${colors.green('✔')}︎ ${title}`);
   } catch (ex) {
-    console.error(`\t✗ ${title}`);
+    console.error('\n' + colors.red('Oh no, we failed some:\n')); // eslint-disable-line
+    console.error(`   ${colors.red('✗')} ${colors.red(title)}\n`);
     throw ex;
   }
 };
